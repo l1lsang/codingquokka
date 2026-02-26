f0ed8b85e35869ad56c806c601558a5f5b0e0232
codingquokka <104996819+l1lsang@users.noreply.github.com>
2026-02-27 00:46:46 +0900

    ASDFSADF

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,11 +5,12 @@ import FeaturedProjects from "../components/home/FeaturedProjects";
 import Strength from "../components/home/Strength";
 import CTA from "../components/home/CTA";
 import "../styles/home.css";
-
+import Declaration from "../components/home/Declaration";
 export default function Home() {
   return (
     <main className="home">
       <Hero />
+       <Declaration />
       <TechStack />
       <FeaturedProjects />
       <Strength />
