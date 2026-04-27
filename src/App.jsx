b1eca3cb39fac469f10ea7e40472218b35986038
l1lsang <jkm0831123@gmail.com>
2026-04-27 13:16:34 +0900

    DASDFASDFASDF

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-import ThemeToggle from "./components/ThemeToggle";
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* 전역 UI (나중에 Header로 이동 가능) */}
-      <div style={{ position: "fixed", top: 20, right: 20, zIndex: 1000 }}>
-        <ThemeToggle />
-      </div>
-
       <Routes>
         <Route path="/" element={<Home />} />
 
